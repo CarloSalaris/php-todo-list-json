906d7c857029be7755845a8da58d448089d3aa4c
CarloSalaris <carloa.salaris@gmail.com>
2023-06-22 20:38:07 +0200

    Can select if task is done (bg + line through)

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,9 +3,10 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      listElements: [],
+      tasks: [],
       newTask: {
-        content: ""
+        text: "",
+        doneTask: false
       }
     }
   },
@@ -20,44 +21,61 @@ export default {
 
       axios.post(url, data, headers)
         .then(response => {
-          this.listElements = response.data;
+          this.tasks = response.data;
 
-          this.newTask.content = "";
+          this.newTask.text = "";
+          this.newTask.doneTask = false;
         })
         .catch(error => console.error("error", error));
+    },
+    doneUndone(index) {
+      this.tasks[index].doneTask = !this.tasks[index].doneTask;
     }
   },
   mounted() {
 
     axios.get('http://localhost/PHP_PROJECTS/php-todo-list-json/php/index.php')
       .then(response => {
-        this.listElements = response.data;
+        this.tasks = response.data;
       });
   }
 }
 </script>
 
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-6">
-        <h1 class="text-center">Todo List</h1>
-        <!-- List -->
-        <div>
-          <ul>
-            <li v-for="(element, idx) in listElements" :key="idx">
-              {{ element.content }}
-            </li>
-          </ul>
-          <form @submit.prevent="onSubmit" class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Inserisci elemento..." aria-label="Recipient's username"
-              aria-describedby="button-addon2" v-model="newTask.content">
-            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Inserisci</button>
-          </form>
+  <div class="container p-3">
+    <h1 class="text-center">Todo List</h1>
 
-        </div>
-      </div>
+    <!-- OUTPUT TASK -->
+    <div class="taskContainer">
+      <ul>
+        <!-- Task element -->
+        <li class="task" v-for="(task, idx) in tasks" :class="(task.doneTask === true) ? 'doneTask' : ''"
+          @click.prevent="doneUndone(idx)">
+
+          <!-- Task text (from input) -->
+          <span>{{ task.text }}</span>
+
+          <!-- "x" delete button -->
+          <button>
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+
+        </li>
+
+        <!-- Empty task list message (hidden) -->
+        <h2 v-if="tasks.length === 0" class="emptyTasks">Non ci sono task al momento...</h2>
+      </ul>
     </div>
+
+    <form @submit.prevent="onSubmit" class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Inserisci elemento..." aria-label="Recipient's username"
+        aria-describedby="button-addon2" v-model="newTask.text">
+      <button class="btn btn-outline-secondary" type="submit" id="button-addon2"
+        @keyup.enter="onSubmit">Inserisci</button>
+    </form>
+
+
   </div>
 </template>
 
