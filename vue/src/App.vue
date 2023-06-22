f0e30d9046204feba2cd49853f36e2c688f3dad5
CarloSalaris <carloa.salaris@gmail.com>
2023-06-22 17:28:30 +0200

    invio task aggiunto alla lista e al file json

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,9 +4,29 @@ export default {
   data() {
     return {
       listElements: [],
+      newTask: {
+        content: ""
+      }
     }
   },
-  methods: {},
+  methods: {
+    onSubmit() {
+
+      const url = 'http://localhost/PHP_PROJECTS/php-todo-list-json/php/postTask.php'
+      const data = this.newTask;
+      const headers = {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      };
+
+      axios.post(url, data, headers)
+        .then(response => {
+          this.listElements = response.data;
+
+          this.newTask.content = "";
+        })
+        .catch(error => console.error("error", error));
+    }
+  },
   mounted() {
 
     axios.get('http://localhost/PHP_PROJECTS/php-todo-list-json/php/index.php')
@@ -28,8 +48,13 @@ export default {
             <li v-for="(element, idx) in listElements" :key="idx">
               {{ element.content }}
             </li>
-
           </ul>
+          <form @submit.prevent="onSubmit" class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Inserisci elemento..." aria-label="Recipient's username"
+              aria-describedby="button-addon2" v-model="newTask.content">
+            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Inserisci</button>
+          </form>
+
         </div>
       </div>
     </div>
