23d91864d3c5838ae44445565d68136cd29eb2d1
CarloSalaris <carloa.salaris@gmail.com>
2023-06-22 16:44:37 +0200

    first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      listElements: [],
+    }
+  },
+  methods: {},
+  mounted() {
+
+    axios.get('http://localhost/PHP_PROJECTS/php-todo-list-json/php/index.php')
+      .then(response => {
+        this.listElements = response.data;
+      });
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-6">
+        <h1 class="text-center">Todo List</h1>
+        <!-- List -->
+        <div>
+          <ul>
+            <li v-for="(element, idx) in listElements" :key="idx">
+              {{ element.content }}
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+</style>
